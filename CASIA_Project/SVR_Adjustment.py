c0101dcb77d460ed65acbf1363fd55874307d071
BaoZhongtian <35563563+BaoZhongtian@users.noreply.github.com>
2018-09-18 21:38:51 +0800

    SVR_Adjust

---
+++ CASIA_Project/SVR_Adjustment.py
@@ -0,0 +1,52 @@
+from sklearn import svm
+import numpy
+import os
+
+if __name__ == '__main__':
+    conf = 'avec2011'
+    loadpath = 'F:\\DataAugment20dB-Features-Npy\\' + conf + '\\'
+    savepath = 'F:\\AVEC-SVR-Changed\\' + conf + '\\'
+    os.makedirs(savepath)
+    trainData = numpy.load(loadpath + 'TrainData.npy')
+    trainLabel = numpy.load(loadpath + 'TrainLabel.npy')
+    developData = numpy.load(loadpath + 'DevelopData.npy')
+    developLabel = numpy.load(loadpath + 'DevelopLabel.npy')
+    testData = numpy.load(loadpath + 'TestData.npy')
+    testLabel = numpy.load(loadpath + 'TestLabel.npy')
+
+    print(numpy.shape(trainData), numpy.shape(trainLabel), numpy.shape(developData), numpy.shape(developLabel),
+          numpy.shape(testData), numpy.shape(testLabel))
+
+    for C in [1, 0.9, 0.8, 0.7, 0.6, 0.5]:
+        for epsilon in [0.5, 0.2, 0.1, 0.05, 0.01]:
+            for gamma in [0.01, 0.005, 0.001, 0.0005, 0.0001]:
+                for tol in [1e-2, 1e-3, 1e-4]:
+                    file = open(
+                        savepath + 'C=' + str(C) + '-epsilon=' + str(epsilon) + '-gamma=' + str(gamma) + '-tol=' + str(
+                            tol) + '.csv', 'w')
+
+                    print('C=' + str(C) + '-epsilon=' + str(epsilon) + '-gamma=' + str(gamma) + '-tol=' + str(tol))
+                    clf = svm.SVR(C=C, epsilon=epsilon, gamma=gamma, tol=tol)
+                    clf.fit(trainData, trainLabel)
+
+                    predict = clf.predict(developData)
+                    MAE, RMSE = 0, 0
+                    for index in range(len(predict)):
+                        MAE += numpy.abs(predict[index] - developLabel[index])
+                        RMSE += (predict[index] - developLabel[index]) * (predict[index] - developLabel[index])
+                    MAE = MAE / len(predict)
+                    RMSE = numpy.sqrt(RMSE / len(predict))
+                    print('Develop Part\t', MAE, '\t', RMSE)
+                    file.write(str(MAE) + ',' + str(RMSE) + ',')
+
+                    predict = clf.predict(testData)
+                    MAE, RMSE = 0, 0
+                    for index in range(len(predict)):
+                        MAE += numpy.abs(predict[index] - testLabel[index])
+                        RMSE += (predict[index] - testLabel[index]) * (predict[index] - testLabel[index])
+                    MAE = MAE / len(predict)
+                    RMSE = numpy.sqrt(RMSE / len(predict))
+                    print('Test Part\t', MAE, '\t', RMSE)
+                    file.write(str(MAE) + ',' + str(RMSE))
+
+                    file.close()
--- CTC_Project/Train/Exp_CTC.py
+++ CTC_Project/Train/Exp_CTC.py
@@ -2,18 +2,23 @@ from CTC_Project.Loader.IEMOCAP_CTC_Loader import IEMOCAP_CTC_Loader
 from CTC_Project.Module.CTC_Reconfiguration import CTC
 import tensorflow
 import numpy
+import os
 from time import strftime
 
 if __name__ == '__main__':
-    trainData, trainLabel, trainSeq, testData, testLabel, testSeq = IEMOCAP_CTC_Loader(bands=30, appoint=0)
+    bands = 120
+    trainData, trainLabel, trainSeq, testData, testLabel, testSeq = IEMOCAP_CTC_Loader(bands=bands, appoint=-1)
+    savepath = 'F:\\CTC-NeuralNetwork\\Bands' + str(bands) + '\\'
     graph = tensorflow.Graph()
     with graph.as_default():
-        classifier = CTC(trainData=trainData, trainLabel=trainLabel, trainSeqLength=trainSeq, featureShape=30,
-                         numClass=6, batchSize=128)
+        classifier = CTC(trainData=trainData, trainLabel=trainLabel, trainSeqLength=trainSeq, featureShape=bands,
+                         numClass=6, batchSize=128, learningRate=1e-5)
         print(classifier.information)
         for episode in range(100):
             loss = classifier.Train()
             print('\rEpisode : ' + str(episode) + '\tLoss : ' + str(loss) + strftime("%Y/%m/%d %H:%M:%S"))
+        os.makedirs(savepath)
+        classifier.Save(savepath=savepath + 'NeuralParameter')
         # classifier.PredictOutput(data=trainData, sequence=trainSeq)
     # 还差保存参数以及计算
     # 目前的计划为使用全部的数据训练出较好的结果之后将数据保存，再进行后续操作
