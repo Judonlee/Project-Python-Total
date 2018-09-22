f4f71037d50f4017ddd565e65e98c71d58254b0d
BaoZhongtian <35563563+BaoZhongtian@users.noreply.github.com>
2018-09-22 14:46:12 +0800

    Final Pooling Part

--- CTC_Project_Again/Loader/IEMOCAP_Loader.py
+++ CTC_Project_Again/Loader/IEMOCAP_Loader.py
@@ -3,33 +3,36 @@ import numpy
 
 
 def IEMOCAP_Loader(loadpath, appoint):
-    trainData, trainLabel, testData, testLabel = [], [], [], []
+    trainData, trainLabel, trainSeq, testData, testLabel, testSeq = [], [], [], [], [], []
     for indexA in os.listdir(loadpath):
         for indexB in os.listdir(loadpath + indexA):
             print('Loading : ', indexA, indexB)
             for indexC in range(1, 6):
                 currentData = numpy.load(loadpath + indexA + '\\' + indexB + '\\Session' + str(indexC) + '-Data.npy')
                 currentLabel = numpy.load(loadpath + indexA + '\\' + indexB + '\\Session' + str(indexC) + '-Label.npy')
+                currentSeq = numpy.load(loadpath + indexA + '\\' + indexB + '\\Session' + str(indexC) + '-Seq.npy')
 
                 if ['Female', 'Male'].index(indexB) * 5 + indexC - 1 == appoint:
                     testData.extend(currentData)
                     testLabel.extend(currentLabel)
+                    testSeq.extend(currentSeq)
                 else:
                     trainData.extend(currentData)
                     trainLabel.extend(currentLabel)
-    print(numpy.shape(trainData), numpy.shape(trainLabel), numpy.sum(trainLabel, axis=0))
-    print(numpy.shape(testData), numpy.shape(testLabel), numpy.sum(testLabel, axis=0))
-    return trainData, trainLabel, testData, testLabel
+                    trainSeq.extend(currentSeq)
+    print(numpy.shape(trainData), numpy.shape(trainLabel), numpy.shape(trainSeq), numpy.sum(trainLabel, axis=0))
+    print(numpy.shape(testData), numpy.shape(testLabel), numpy.shape(testSeq), numpy.sum(testLabel, axis=0))
+    return trainData, trainLabel, trainSeq, testData, testLabel, testSeq
 
 
 if __name__ == '__main__':
-    loadpath = 'F:\\Project-CTC-Data\\Csv\\Bands120\\'
-    savepath = 'F:\\Project-CTC-Data\\Npy\\Bands120\\'
+    loadpath = 'F:\\Project-CTC-Data\\Csv\\Bands30\\'
+    savepath = 'F:\\Project-CTC-Data\\Npy\\Bands30\\'
 
     for indexA in os.listdir(loadpath):
         for indexB in os.listdir(loadpath + indexA):
             for indexC in os.listdir(loadpath + indexA + '\\' + indexB):
-                currentData, currentLabel = [], []
+                currentData, currentLabel, currentSeq = [], [], []
 
                 for indexD in os.listdir(loadpath + indexA + '\\' + indexB + '\\' + indexC):
                     for indexE in os.listdir(loadpath + indexA + '\\' + indexB + '\\' + indexC + '\\' + indexD):
@@ -47,8 +50,10 @@ if __name__ == '__main__':
 
                         currentData.append(treatData)
                         currentLabel.append(treatLabel)
+                        currentSeq.append(len(treatData))
 
                 if not os.path.exists(savepath + indexA + '\\' + indexB):
                     os.makedirs(savepath + indexA + '\\' + indexB)
                 numpy.save(savepath + indexA + '\\' + indexB + '\\' + indexC + '-Data.npy', currentData)
                 numpy.save(savepath + indexA + '\\' + indexB + '\\' + indexC + '-Label.npy', currentLabel)
+                numpy.save(savepath + indexA + '\\' + indexB + '\\' + indexC + '-Seq.npy', currentSeq)
--- CTC_Project_Again/Train/Exp_FinalPooling.py
+++ CTC_Project_Again/Train/Exp_FinalPooling.py
@@ -1,5 +1,17 @@
 from CTC_Project_Again.Loader.IEMOCAP_Loader import IEMOCAP_Loader
+from CTC_Project_Again.Model.LSTM_FinalPooling import LSTM_FinalPooling
 
 if __name__ == '__main__':
-    trainData, trainLabel, testData, testLabel = IEMOCAP_Loader(loadpath='F:\\Project-CTC-Data\\Npy\\Bands30\\',
-                                                                appoint=0)
+    trainData, trainLabel, trainSeq, testData, testLabel, testSeq = \
+        IEMOCAP_Loader(loadpath='F:\\Project-CTC-Data\\Npy\\Bands30\\', appoint=0)
+    classifier = LSTM_FinalPooling(trainData=trainData, trainLabel=trainLabel, trainSeqLength=trainSeq, featureShape=30,
+                                   numClass=4)
+    print(classifier.information)
+    for episode in range(100):
+        loss = classifier.Train()
+        print('\rEpisode %d/%d Total Loss : %f' % (episode, 100, loss))
+        print('Train Part :')
+        print(classifier.Test(testData=trainData, testLabel=trainLabel, testSeq=trainSeq))
+        print('Test Part :')
+        print(classifier.Test(testData=testData, testLabel=testLabel, testSeq=testSeq))
+        print('\n')
