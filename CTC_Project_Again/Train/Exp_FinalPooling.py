2d6e50797f925dadfb91633f60ecb198008ad586
BaoZhongtian <35563563+BaoZhongtian@users.noreply.github.com>
2018-09-27 18:52:07 +0800

    Final Pooling Completed

--- CTC_Project_Again/Train/Exp_FinalPooling.py
+++ CTC_Project_Again/Train/Exp_FinalPooling.py
@@ -1,6 +1,5 @@
 from CTC_Project_Again.Loader.IEMOCAP_Loader import IEMOCAP_Loader
-from CTC_Project_Again.Model.LSTM_FinalPooling import LSTM_FinalPooling
-from CTC_Project_Again.Model.LSTM_AveragePooling import LSTM_AveragePooling
+from CTC_Project_Again.Model.BLSTM_FinalPooling import BLSTM_FinalPooling
 import tensorflow
 from __Base.DataClass import DataClass_TrainTest_Sequence
 from CTC_Project_Again.Engine.TrainTestEngine import TrainTestEngine
@@ -10,15 +9,18 @@ if __name__ == '__main__':
     # os.environ['CUDA_VISIBLE_DEVICES'] = '3'
 
     for bands in [30, 40, 60, 80, 100, 120]:
-        for appoint in range(1, 10, 2):
-            savepath = 'F:/Project-CTC-Data/Records-AveragePooing/Bands-' + str(bands) + '-' + str(appoint) + '/'
+        for appoint in range(10):
+            savepath = 'D:/ProjectData/Project-CTC-Data/Records-FinalPooling/Bands-' + str(bands) \
+                       + '-' + str(appoint) + '/'
             graph = tensorflow.Graph()
             with graph.as_default():
                 trainData, trainLabel, trainSeq, testData, testLabel, testSeq = \
-                    IEMOCAP_Loader(loadpath='F:/Project-CTC-Data/Npy/Bands' + str(bands) + '/', appoint=appoint)
+                    IEMOCAP_Loader(loadpath='D:/ProjectData/Project-CTC-Data/Npy-Normalized/Bands' + str(bands) + '/',
+                                   appoint=appoint)
                 dataClass = DataClass_TrainTest_Sequence(trainData=trainData, trainLabel=trainLabel, trainSeq=trainSeq,
                                                          testData=testData, testLabel=testLabel, testSeq=testSeq)
-                classifier = LSTM_AveragePooling(trainData=trainData, trainLabel=trainLabel, trainSeqLength=trainSeq,
-                                                 featureShape=bands, numClass=4, rnnLayers=1)
+                classifier = BLSTM_FinalPooling(trainData=trainData, trainLabel=trainLabel, trainSeqLength=trainSeq,
+                                                featureShape=bands, numClass=4, learningRate=1e-4, batchSize=128)
                 print(classifier.information)
+                # exit()
                 TrainTestEngine(dataClass=dataClass, classifier=classifier, totalEpoch=100, savepath=savepath)
