f18732753c59a48bebf1106804625164097374a6
BaoZhongtian <35563563+BaoZhongtian@users.noreply.github.com>
2018-12-18 09:32:31 +0800

    A Lot Delete

--- CASIA_Project/Utils/DataClass.py
+++
@@ -1,8 +0,0 @@
-class DataClass:
-    def __init__(self, trainData, trainLabel, developData, developLabel, testData, testLabel):
-        self.trainData = trainData
-        self.trainLabel = trainLabel
-        self.developData = developData
-        self.developLabel = developLabel
-        self.testData = testData
-        self.testLabel = testLabel
