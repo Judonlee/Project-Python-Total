be4d259748c85c6bd646af334ad96e6846418882
BaoZhongtian <35563563+BaoZhongtian@users.noreply.github.com>
2019-03-19 10:54:38 +0800

    Name Conflict

--- DepressionRecognition/Model/AutoEncoder.py
+++ DepressionRecognition/Model/AutoEncoder.py
@@ -45,105 +45,108 @@ class AutoEncoder(NeuralNetwork_Base):
         # Encoder
         ###################################################################################################
 
-        with tensorflow.variable_scope('Encoder'):
-            self.parameters['Encoder_Cell_Forward'] = tensorflow.nn.rnn_cell.MultiRNNCell(
+        with tensorflow.variable_scope('Encoder_AE'):
+            self.parameters['Encoder_Cell_Forward_AE'] = tensorflow.nn.rnn_cell.MultiRNNCell(
                 cells=[rnn.LSTMCell(num_units=self.hiddenNodules) for _ in range(self.rnnLayers)], state_is_tuple=True)
-            self.parameters['Encoder_Cell_Backward'] = tensorflow.nn.rnn_cell.MultiRNNCell(
+            self.parameters['Encoder_Cell_Backward_AE'] = tensorflow.nn.rnn_cell.MultiRNNCell(
                 cells=[rnn.LSTMCell(num_units=self.hiddenNodules) for _ in range(self.rnnLayers)], state_is_tuple=True)
 
-            self.parameters['Encoder_Output'], self.parameters['Encoder_FinalState'] = \
+            self.parameters['Encoder_Output_AE'], self.parameters['Encoder_FinalState_AE'] = \
                 tensorflow.nn.bidirectional_dynamic_rnn(
-                    cell_fw=self.parameters['Encoder_Cell_Forward'], cell_bw=self.parameters['Encoder_Cell_Backward'],
+                    cell_fw=self.parameters['Encoder_Cell_Forward_AE'],
+                    cell_bw=self.parameters['Encoder_Cell_Backward_AE'],
                     inputs=self.dataInput, sequence_length=self.seqInput, dtype=tensorflow.float32)
 
         if self.attention is None:
-            self.parameters['Decoder_InitalState'] = []
+            self.parameters['Decoder_InitalState_AE'] = []
             for index in range(self.rnnLayers):
-                self.parameters['Encoder_Cell_Layer%d' % index] = rnn.LSTMStateTuple(
-                    c=tensorflow.concat([self.parameters['Encoder_FinalState'][index][0].c,
-                                         self.parameters['Encoder_FinalState'][index][1].c], axis=1),
-                    h=tensorflow.concat([self.parameters['Encoder_FinalState'][index][0].h,
-                                         self.parameters['Encoder_FinalState'][index][1].h], axis=1))
-                self.parameters['Decoder_InitalState'].append(self.parameters['Encoder_Cell_Layer%d' % index])
-            self.parameters['Decoder_InitalState'] = tuple(self.parameters['Decoder_InitalState'])
+                self.parameters['Encoder_Cell_Layer%d_AE' % index] = rnn.LSTMStateTuple(
+                    c=tensorflow.concat([self.parameters['Encoder_FinalState_AE'][index][0].c,
+                                         self.parameters['Encoder_FinalState_AE'][index][1].c], axis=1),
+                    h=tensorflow.concat([self.parameters['Encoder_FinalState_AE'][index][0].h,
+                                         self.parameters['Encoder_FinalState_AE'][index][1].h], axis=1))
+                self.parameters['Decoder_InitalState_AE'].append(self.parameters['Encoder_Cell_Layer%d_AE' % index])
+            self.parameters['Decoder_InitalState_AE'] = tuple(self.parameters['Decoder_InitalState_AE'])
         else:
-            self.attentionList = self.attention(dataInput=self.parameters['Encoder_Output'],
+            self.attentionList = self.attention(dataInput=self.parameters['Encoder_Output_AE'],
                                                 scopeName=self.attentionName, hiddenNoduleNumber=2 * self.hiddenNodules,
                                                 attentionScope=self.attentionScope, blstmFlag=True)
-            self.parameters['Decoder_InitalState'] = []
+            self.parameters['Decoder_InitalState_AE'] = []
             for index in range(self.rnnLayers):
-                self.parameters['Encoder_Cell_Layer%d' % index] = rnn.LSTMStateTuple(
+                self.parameters['Encoder_Cell_Layer%d_AE' % index] = rnn.LSTMStateTuple(
                     c=self.attentionList['FinalResult'],
                     h=tensorflow.concat(
-                        [self.parameters['Encoder_FinalState'][index][0].h,
-                         self.parameters['Encoder_FinalState'][index][1].h],
+                        [self.parameters['Encoder_FinalState_AE'][index][0].h,
+                         self.parameters['Encoder_FinalState_AE'][index][1].h],
                         axis=1))
-                self.parameters['Decoder_InitalState'].append(self.parameters['Encoder_Cell_Layer%d' % index])
-            self.parameters['Decoder_InitalState'] = tuple(self.parameters['Decoder_InitalState'])
+                self.parameters['Decoder_InitalState_AE'].append(self.parameters['Encoder_Cell_Layer%d_AE' % index])
+            self.parameters['Decoder_InitalState_AE'] = tuple(self.parameters['Decoder_InitalState_AE'])
 
         #############################################################################
         # Decoder Label Pretreatment
         #############################################################################
 
-        self.parameters['Decoder_Helper'] = seq2seq.TrainingHelper(
-            inputs=self.dataInput, sequence_length=self.seqInput, name='Decoder_Helper')
-        with tensorflow.variable_scope('Decoder'):
-            self.parameters['Decoder_FC'] = Dense(self.featureShape)
-            self.parameters['Decoder_Cell'] = tensorflow.nn.rnn_cell.MultiRNNCell(
+        self.parameters['Decoder_Helper_AE'] = seq2seq.TrainingHelper(
+            inputs=self.dataInput, sequence_length=self.seqInput, name='Decoder_Helper_AE')
+        with tensorflow.variable_scope('Decoder_AE'):
+            self.parameters['Decoder_FC_AE'] = Dense(self.featureShape)
+            self.parameters['Decoder_Cell_AE'] = tensorflow.nn.rnn_cell.MultiRNNCell(
                 cells=[rnn.LSTMCell(num_units=self.hiddenNodules * 2) for _ in range(self.rnnLayers)],
                 state_is_tuple=True)
 
-            self.parameters['Decoder'] = seq2seq.BasicDecoder(cell=self.parameters['Decoder_Cell'],
-                                                              helper=self.parameters['Decoder_Helper'],
-                                                              initial_state=self.parameters['Decoder_InitalState'],
-                                                              output_layer=self.parameters['Decoder_FC'])
+            self.parameters['Decoder_AE'] = seq2seq.BasicDecoder(
+                cell=self.parameters['Decoder_Cell_AE'], helper=self.parameters['Decoder_Helper_AE'],
+                initial_state=self.parameters['Decoder_InitalState_AE'], output_layer=self.parameters['Decoder_FC_AE'])
 
-            self.parameters['Decoder_Logits'], self.parameters['Decoder_FinalState'], self.parameters[
-                'Decoder_FinalSeq'] = seq2seq.dynamic_decode(decoder=self.parameters['Decoder'])
+            self.parameters['Decoder_Logits_AE'], self.parameters['Decoder_FinalState_AE'], self.parameters[
+                'Decoder_FinalSeq_AE'] = seq2seq.dynamic_decode(decoder=self.parameters['Decoder_AE'])
 
         #############################################################################
         # Losses
         #############################################################################
 
-        self.parameters['Loss'] = tensorflow.losses.absolute_difference(
-            labels=self.dataInput, predictions=self.parameters['Decoder_Logits'][0], weights=self.weight)
-        self.train = tensorflow.train.AdamOptimizer(learning_rate=learningRate).minimize(self.parameters['Loss'])
+        self.parameters['Loss_AE'] = tensorflow.losses.absolute_difference(
+            labels=self.dataInput, predictions=self.parameters['Decoder_Logits_AE'][0], weights=self.weight)
+        self.train = tensorflow.train.AdamOptimizer(learning_rate=learningRate).minimize(self.parameters['Loss_AE'])
 
-    def Train(self):
+    def Train(self, logname):
         data, seq = Shuffle_Train(data=self.treatData, label=self.treatSeq)
         # data, seq = self.treatData, self.treatSeq
 
         # for index in range(len(data)):
         #     print(numpy.shape(data[index]), seq[index])
 
-        startPosition = 0
-        totalLoss = 0.0
-        while startPosition < numpy.shape(data)[0]:
-            batchData, batchSeq = [], []
+        with open(logname, 'w') as file:
+            startPosition = 0
+            totalLoss = 0.0
+            while startPosition < numpy.shape(data)[0]:
+                batchData, batchSeq = [], []
 
-            maxSeq = min(numpy.max(seq[startPosition:startPosition + self.batchSize]), self.maxLen)
-            for index in range(startPosition, min(startPosition + self.batchSize, numpy.shape(data)[0])):
-                batchSeq.append(min(seq[index], self.maxLen))
+                maxSeq = min(numpy.max(seq[startPosition:startPosition + self.batchSize]), self.maxLen)
+                for index in range(startPosition, min(startPosition + self.batchSize, numpy.shape(data)[0])):
+                    batchSeq.append(min(seq[index], self.maxLen))
 
-            for index in range(startPosition, min(startPosition + self.batchSize, numpy.shape(data)[0])):
-                if numpy.shape(data[index])[0] >= self.maxLen:
-                    currentData = data[index][0:self.maxLen]
-                else:
-                    currentData = numpy.concatenate(
-                        [data[index], numpy.zeros([maxSeq - numpy.shape(data[index])[0], numpy.shape(data[index])[1]])],
-                        axis=0)
-                # print(numpy.shape(currentData))
-                batchData.append(currentData)
+                for index in range(startPosition, min(startPosition + self.batchSize, numpy.shape(data)[0])):
+                    if numpy.shape(data[index])[0] >= self.maxLen:
+                        currentData = data[index][0:self.maxLen]
+                    else:
+                        currentData = numpy.concatenate(
+                            [data[index],
+                             numpy.zeros([maxSeq - numpy.shape(data[index])[0], numpy.shape(data[index])[1]])],
+                            axis=0)
+                    # print(numpy.shape(currentData))
+                    batchData.append(currentData)
 
-            # print(startPosition, numpy.shape(batchData), numpy.shape(batchSeq), maxSeq)
-            # print(batchSeq, '\n')
+                # print(startPosition, numpy.shape(batchData), numpy.shape(batchSeq), maxSeq)
+                # print(batchSeq, '\n')
 
-            loss, _ = self.session.run(fetches=[self.parameters['Loss'], self.train],
-                                       feed_dict={self.dataInput: batchData, self.seqInput: batchSeq})
-            totalLoss += loss
-            print('\rBatch %d/%d Loss = %f' % (startPosition, len(data), loss), end='')
+                loss, _ = self.session.run(fetches=[self.parameters['Loss_AE'], self.train],
+                                           feed_dict={self.dataInput: batchData, self.seqInput: batchSeq})
+                file.write(str(loss) + '\n')
+                totalLoss += loss
+                print('\rBatch %d/%d Loss = %f' % (startPosition, len(data), loss), end='')
 
-            # exit()
+                # exit()
 
-            startPosition += self.batchSize
+                startPosition += self.batchSize
         return totalLoss
--- Test.py
+++ Test.py
@@ -8,9 +8,23 @@
 # for key in var_to_shape_map:
 #     print('tensor_name: ', key)
 
+import matplotlib.pylab as plt
 import numpy
 
 if __name__ == '__main__':
-    data = numpy.load(r'D:\LIDC\LBP-Npy\R=1_P=4\Part0-Label.npy')
-    print(data)
-    print(numpy.shape(data))
+    # name = ['Standard Attention', 'Local Attention(Scope = 1)', 'Local Attention(Scope = 2)', 'Monotonic Attention',
+    #         'Monotonic Chunkwise Attention']
+    name = ['Without Attention']
+    # part = ['SA-0', 'LA-1', 'LA-2', 'MA-10', 'MCA-10']
+    part = ['None-0']
+    for partIndex in range(len(part)):
+        loadpath = 'E:/ProjectData_Depression/Experiment/AutoEncoder-New/%s/' % part[partIndex]
+        totalData = []
+        for index in range(20):
+            data = numpy.genfromtxt(fname=loadpath + '%04d.csv' % index, dtype=float, delimiter=',')
+            totalData.extend(data)
+        plt.plot(totalData, label=name[partIndex])
+    plt.xlabel('Training Batch Number')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
